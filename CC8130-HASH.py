d1ac64d7bc3ee69b4204cf2a2f533a316bbb6b4f
Pedro <72952088+Batacoito@users.noreply.github.com>
2023-10-04 22:02:58 -0300

    Add files via upload

---
+++ CC8130-HASH.py
@@ -0,0 +1,18 @@
+import hashlib
+
+myText = 'Somente SHA256 correto'
+SHA256_certo = hashlib.sha256(myText.encode('utf-8')).hexdigest()
+MD5_certo = hashlib.md5(myText.encode('utf-8')).hexdigest()
+
+SHA256_teorico = "5b22f6bf621c2116f0d4589c3b5405ed3b5d768b9ba1dfafbae9292331ce9827"
+MD5_teorico = "cfe69ac7a0b07810b391c27b1ea838cd"
+
+print("SHA256 = " + SHA256_certo)
+print("MD5 = " + MD5_certo)
+
+if (SHA256_teorico == SHA256_certo):
+    print("SHA256 certo")
+if (MD5_certo == MD5_teorico):
+    print("MD5 certo")
+
+
